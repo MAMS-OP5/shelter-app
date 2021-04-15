c15b550bb0339cc3b8e5c035903205ed918e93da
caramurphyy <murphylcara@gmail.com>
2021-04-15 01:36:54 -0400

    first commit

---
+++ app/src/main/java/com/example/afgapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.afgapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button lookShelter = (Button) findViewById(R.id.LookForShelterButton);
+
+        lookShelter.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, ShelterPov.class));
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/afgapp/ShelterPov.java
@@ -0,0 +1,30 @@
+package com.example.afgapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class ShelterPov extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelter_pov);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
