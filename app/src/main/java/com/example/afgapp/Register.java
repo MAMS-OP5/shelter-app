ceba4baeca4b9dbeb813900713b32b2616dbb6bc
stsingh <sahej3526@gmail.com>
2021-04-19 12:52:02 -0400

    new commits

--- app/src/main/java/com/example/afgapp/Register.java
+++ app/src/main/java/com/example/afgapp/Register.java
@@ -2,6 +2,7 @@ package com.example.afgapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,17 +14,28 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
     EditText fName, fEmail, fPassword, fPhoneNum;
     Button fRegisterBtn;
     TextView fLoginBtn;
     Button backBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,7 @@ public class Register extends AppCompatActivity {
         fLoginBtn = findViewById(R.id.createText);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
         backBtn = findViewById(R.id.backBtn);
@@ -57,6 +70,8 @@ public class Register extends AppCompatActivity {
             public void onClick(View v) {
                 String email = fEmail.getText().toString().trim();
                 String password = fPassword.getText().toString().trim();
+                String phone = fPhoneNum.getText().toString().trim();
+                String facName = fName.getText().toString().trim();
 
                 if(email.isEmpty()) {
                     fEmail.setError("Email is Required");
@@ -81,6 +96,24 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fName", facName);
+                            user.put("email", email);
+                            user.put("phone", phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: user profile created for: " + userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure : " + e.toString());
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(), ShelterPov.class));
                         }else {
                             Toast.makeText(Register.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/afgapp/ShelterPov.java
+++ app/src/main/java/com/example/afgapp/ShelterPov.java
@@ -1,6 +1,7 @@
 package com.example.afgapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,19 +9,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class ShelterPov extends AppCompatActivity {
-
+    TextView name, email, phone, resources, address;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_pov);
+        phone = findViewById(R.id.shelterPhoneDisplay);
+        name = findViewById(R.id.shelterName);
+        email = findViewById(R.id.shelterEmailDisplay);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                phone.setText(documentSnapshot.getString("phone"));
+                name.setText(documentSnapshot.getString("fName"));
+                email.setText(documentSnapshot.getString("email"));
+            }
+        });
     }
 
     public void logout(View view) {
