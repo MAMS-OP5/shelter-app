106afd6de32df701e5b9669a926e1845d0e19d91
sahej <sahej3526@gmail.com>
2021-04-21 13:39:35 -0400

    more small changes

--- app/src/main/java/com/example/afgapp/UpdateInfo.java
+++ app/src/main/java/com/example/afgapp/UpdateInfo.java
@@ -1,4 +1,25 @@
 package com.example.afgapp;
 
-public class UpdateInfo {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateInfo extends AppCompatActivity {
+    Button backBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_info);
+        backBtn = findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ShelterPov.class));
+            }
+        });
+    }
 }
